c7295169300e7bfcf79ec37b0aa7da637b550ef1
Abdelrhman Ashraf <abdoashraf938@yahoo.com>
2025-01-21 18:26:00 +0200

    Refactor workflow: update argument parsing to use model and training configuration files, and improve dataset handling

--- workflow.py
+++ workflow.py
@@ -1,17 +1,16 @@
 import pandas as pd
 from Models.AutoModel import get_model
-from Models.Trainer import Trainer
-from Models.TrainingArguments import TrainingArguments
+from Training.Trainer import Trainer
+from Training.TrainingArguments import TrainingArguments
 from Tokenizers.Tokenizers import Callable_tokenizer
 from Models.ModelArgs import ModelArgs
-from train import train_model
 import os
 import argparse
 import sys
-from config import *
 import torch
 
 from utils.data_utils import MT_Dataset, MYCollate
+from utils.model_utils import get_parameters_info
 # import onnx
 
 ## Data params: train_csv_path, valid_csv_path, batch_size, num_workers, seed, device, out_dir, maxlen
@@ -26,25 +25,9 @@ def parse_arguments():
     parser.add_argument('--valid_csv_path', type=str, required=True, help='CSV of columns "source_lang" and "target_lang" for validation')
     parser.add_argument('--src_tokenizer_path', type=str, required=True, help='Path of source tokenizer model')
     parser.add_argument('--trg_tokenizer_path', type=str, required=True, help='Path of target tokenizer model')
-    parser.add_argument('--out_dir', type=str, required=True, help='Working dir')
-    parser.add_argument('--model_type', choices=['s2s', 's2sAttention', 'transformer'], required=True, help='needed model architecture')
-    parser.add_argument('--model_name', type=str, required=True, help='Model prefix name')
-
-    parser.add_argument('--batch_size', type=int, default=DEFAULT_BATCH_SIZE, help='Batch size')
-    parser.add_argument('--num_workers', type=int, default=DEFAULT_NUM_WORKERS, help='number of workers for dataloader')
-    parser.add_argument('--seed', type=int, default=DEFAULT_SEED, help='Random seed')
-    parser.add_argument('--maxlen', type=int, default=DEFAULT_MAXLEN, help='Maximum length of input sequence')
-
-    parser.add_argument('--epochs', type=int, default=DEFAULT_EPOCHS, help='number of training epochs')
-    parser.add_argument('--dim_embed', type=int, default=DEFAULT_DIM_EMBED, help='dims of embedding matrix')
-    parser.add_argument('--dim_model', type=int, default=DEFAULT_DIM_MODEL, help='dims of the model')
-    parser.add_argument('--dim_feedforward', type=int, default=DEFAULT_DIM_FEEDFORWARD, help='dims of feedforward network at the model')
-    parser.add_argument('--num_layers', type=int, default=DEFAULT_NUM_LAYERS, help='number of layers for encoder and decoder')
-    parser.add_argument('--dropout', type=float, default=DEFAULT_DROPOUT, help='proba of all dropout layers')
-    parser.add_argument('--learning_rate', type=float, default=DEFAULT_LEARNING_RATE, help='learning rate')
-    parser.add_argument('--weight_decay', type=float, default=DEFAULT_WEIGHT_DECAY, help='weight decay')
-    parser.add_argument('--device', choices=['cpu', 'cuda'], default=DEFAULT_DEVICE, help='needed model architecture')
-    parser.add_argument('--in_onnx', type=bool, default=DEFAULT_IN_ONNX, help='Save the model in onnx (.onnx) or in pytorch (.pth) format')
+    parser.add_argument('--model_config_path', type=str, required=True, help='A path for model configuration file')
+    parser.add_argument('--training_config_path', type=str, required=True, help='A path for training configuration file')
+    
 
     return parser
  
@@ -61,9 +44,9 @@ if __name__ == '__main__':
     assert os.path.exists(args.valid_csv_path), f"{args.valid_csv_path} : Valid csv not found."
     assert os.path.exists(args.src_tokenizer_path), f"{args.src_tokenizer_path} : Tokenizer.model not found."
     assert os.path.exists(args.trg_tokenizer_path), f"{args.trg_tokenizer_path} : Tokenizer.model not found."
-    if not os.path.exists(args.out_dir): os.makedirs(args.out_dir, exist_ok=True)
+    assert os.path.exists(args.model_config_path), f"{args.model_config_path} : Model configuration file not found."
+    assert os.path.exists(args.training_config_path), f"{args.training_config_path} : Training configuration file not found."
     
-
 
     print("Starting Tokenizers Loading...")
     src_tokenizer = Callable_tokenizer(args.src_tokenizer_path)
@@ -75,32 +58,26 @@ if __name__ == '__main__':
     train_df = pd.read_csv(args.train_csv_path)
     valid_df = pd.read_csv(args.valid_csv_path)
 
-    train_ds = MT_Dataset(src_sentences_list=train_df[0], trg_sentences_list=train_df[1],
+    train_ds = MT_Dataset(src_sentences_list=train_df[train_df.columns[0]], trg_sentences_list=train_df[train_df.columns[1]],
                           src_tokenizer=src_tokenizer, trg_tokenizer=trg_tokenizer)
-    valid_ds = MT_Dataset(src_sentences_list=valid_df[0], trg_sentences_list=valid_df[1],
+    valid_ds = MT_Dataset(src_sentences_list=valid_df[valid_df.columns[0]], trg_sentences_list=valid_df[valid_df.columns[1]],
                           src_tokenizer=src_tokenizer, trg_tokenizer=trg_tokenizer)
-    
     mycollate = MYCollate(batch_first=True, pad_value=-100)
     
-    model_args = ModelArgs(model_type=args.model_type,
-                           model_name=args.model_name,
-                           dim_embed=args.dim_embed,
-                           dim_model=args.dim_model,
-                           dim_feedforward=args.dim_feedforward,
-                           num_layers=args.num_layers,
-                           dropout=args.dropout)
-    model = get_model(model_args, src_vocab_size, trg_vocab_size, -100, args.maxlen)
+    model_args = ModelArgs(config_path=args.model_config_path)
+    model = get_model(model_args, src_vocab_size, trg_vocab_size, mycollate.pad_value)
 
-    training_args = TrainingArguments()
+    names, tr, nontr = get_parameters_info(model=model)
+    print(f"{'Module':<15}{'Trainable':>15}{'Non-Trainable':>15}")
+    for n, ttp, ntp in zip(names, tr, nontr):
+        print(f"{n:<15}{ttp:>15,}{ntp:>15,}")
+    
+    training_args = TrainingArguments(args.training_config_path)
+
     trainer = Trainer(args=training_args, model=model,
                       train_ds=train_df, valid_ds=valid_df,
-                      collator=MYCollate(), compute_metrics_func=None)
+                      collator=mycollate,
+                      compute_metrics_func=None)
     
     train_losses, valid_losses = trainer.train()
-
-
-
-    # train_class_losses, val_class_losses, model, optim, epochs = train_model(dp, args.src_tokenizer_path, args.trg_tokenizer_path)
-    
-    
-        
+  
