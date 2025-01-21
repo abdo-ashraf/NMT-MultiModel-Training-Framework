3f020f6a1917fcf1556be3a90ca79f3636d805fe
Abdelrhman Ashraf <abdoashraf938@yahoo.com>
2025-01-21 04:12:28 +0200

    Add tokenizer training workflow and configuration files

---
+++ Tokenizers/Tokenizers.py
@@ -0,0 +1,46 @@
+import sentencepiece as spm
+import pandas as pd
+## requirements
+# !pip -q install sentencepiece
+
+
+def train(train_csv_path:str, src_lang_tokenizer_params:dict, trg_lang_tokenizer_params:dict):
+  print("Starting Tokenizers Train...")
+  
+  df_train = pd.read_csv(train_csv_path)
+  src_sentences, trg_sentences = df_train['source_lang'], df_train['target_lang']
+
+  spm.SentencePieceTrainer.train(
+      sentence_iterator=iter(src_sentences), **src_lang_tokenizer_params)
+  print(f"{src_lang_tokenizer_params['model_prefix']} Done")
+
+  spm.SentencePieceTrainer.train(
+      sentence_iterator=iter(trg_sentences), **trg_lang_tokenizer_params)
+  print(f"{trg_lang_tokenizer_params['model_prefix']} Done")
+
+  print("Tokenizers Train Done.")
+
+
+## Tokenizer
+class Callable_tokenizer():
+    def __init__(self, tokenizer_path):
+        self.path = tokenizer_path
+        self.tokenizer = spm.SentencePieceProcessor()
+        self.tokenizer.load(tokenizer_path)
+    def __call__(self, text):
+        return self.tokenizer.Encode(text)
+
+    def get_tokenId(self, token_name):
+        return self.tokenizer.piece_to_id(token_name)
+
+    def get_tokenName(self, id):
+        return self.tokenizer.id_to_piece(id)
+
+    def decode(self, tokens_list):
+        return self.tokenizer.Decode(tokens_list)
+
+    def __len__(self):
+        return len(self.tokenizer)
+
+    def user_tokenization(self, text):
+        return [self.get_tokenId('<s>')] + self(text) + [self.get_tokenId('</s>')]
---
+++ Tokenizers/tokenizers_workflow.py
@@ -0,0 +1,34 @@
+import argparse
+from Tokenizers import Tokenizers
+import os
+import json
+
+#####-----Parameters-----#####
+DEFAULT_CONFIG_PATH = 'tokenizers_config.json'
+# DEFAULT_LOG_PATH = None
+
+
+# Command-Line Arguments
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Argument Parser')
+
+    parser.add_argument('--train_csv_path', type=str, required=True, help='CSV of columns "source_lang" and "target_lang" for train')
+    parser.add_argument('--config_path', type=str, default=DEFAULT_CONFIG_PATH, help='Path to the config file of the tokenizers')
+    # parser.add_argument('--log_path', type=str, default=DEFAULT_LOG_PATH, help='Path to the log file')
+
+    return parser
+ 
+
+if __name__ == '__main__':
+    # Argument parsing and validation
+    parser = parse_arguments()
+    args = parser.parse_args()
+    assert os.path.exists(args.train_csv_path), f"{args.train_csv_path} : Train csv not found."
+    assert os.path.exists(args.config_path), f"{args.config_path} : Config file not found."
+    
+    data = json.load(open(args.config_path, 'r'))
+    src_params, tgr_params = data['src_tokenizer'], data['trg_tokenizer']
+    
+    Tokenizers.train(train_csv_path=args.train_csv_path,
+                     src_lang_tokenizer_params=src_params,
+                     trg_lang_tokenizer_params=tgr_params)
