e08229fd4165803f484b7a8832df1ee4183364f7
Abdelrhman Ashraf <abdoashraf938@yahoo.com>
2025-01-22 17:11:17 +0200

    Refactor MT_Dataset to separate target inputs for forward path and loss calculation; update collator and add BLEU score computation function

--- utils.py
+++ utils.py
@@ -4,12 +4,13 @@ from torch.nn.utils.rnn import pad_sequence
 from Tokenizers.Tokenizers import Callable_tokenizer
 import matplotlib.pyplot as plt
 import os
+from nltk.translate.bleu_score import sentence_bleu
 
 
 ## Dataset
 class MT_Dataset(Dataset):
 
-    def __init__(self, src_sentences_list, trg_sentences_list, src_tokenizer:Callable_tokenizer, trg_tokenizer:Callable_tokenizer, reversed_input=False):
+    def __init__(self, src_sentences_list:list, trg_sentences_list:list, src_tokenizer:Callable_tokenizer, trg_tokenizer:Callable_tokenizer, reversed_input=False):
         super(MT_Dataset, self).__init__()
 
         assert len(src_sentences_list) == len(trg_sentences_list), (f"Lengths mismatched: input has {len(src_sentences_list)} sentences, "f"but target has {len(trg_sentences_list)} sentences.")
@@ -28,14 +29,16 @@ class MT_Dataset(Dataset):
         input, target = self.src_sentences_list[index], self.trg_sentences_list[index]
 
         input_numrical_tokens = [self.src_tokenizer.get_tokenId('<s>')] + self.src_tokenizer(input) + [self.src_tokenizer.get_tokenId('</s>')]
-        target_numrical_tokens = [self.trg_tokenizer.get_tokenId('<s>')] +  self.trg_tokenizer(target) + [self.trg_tokenizer.get_tokenId('</s>')]
+        target_numrical_tokens_forward = [self.trg_tokenizer.get_tokenId('<s>')] +  self.trg_tokenizer(target)
+        target_numrical_tokens_loss = self.trg_tokenizer(target) + [self.trg_tokenizer.get_tokenId('</s>')]
         
         input_tensor_tokens = torch.tensor(input_numrical_tokens)
-        target_tensor_tokens = torch.tensor(target_numrical_tokens)
+        target_tensor_tokens_forward = torch.tensor(target_numrical_tokens_forward)
+        target_tensor_tokens_loss = torch.tensor(target_numrical_tokens_loss)
 
         if self.reversed_input: input_tensor_tokens = input_tensor_tokens.flip(0)
 
-        return input_tensor_tokens, target_tensor_tokens
+        return input_tensor_tokens, target_tensor_tokens_forward, target_tensor_tokens_loss
  
     
 ## Collator
@@ -45,14 +48,17 @@ class MYCollate():
         self.batch_first = batch_first
 
     def __call__(self, data):
-        en_stentences = [ex[0] for ex in data]
-        ar_stentences = [ex[1] for ex in data]
+        src_stentences = [ex[0] for ex in data]
+        trg_stentences_forward = [ex[1] for ex in data]
+        trg_stentences_loss = [ex[2] for ex in data]
 
-        padded_en_stentences = pad_sequence(en_stentences, batch_first=self.batch_first,
+        padded_src_stentences = pad_sequence(src_stentences, batch_first=self.batch_first,
                                                       padding_value=self.pad_value)
-        padded_ar_stentences = pad_sequence(ar_stentences, batch_first=self.batch_first,
+        padded_trg_stentences_forward = pad_sequence(trg_stentences_forward, batch_first=self.batch_first,
                                                       padding_value=self.pad_value)
-        return padded_en_stentences, padded_ar_stentences
+        padded_trg_stentences_loss = pad_sequence(trg_stentences_loss, batch_first=self.batch_first,
+                                                      padding_value=self.pad_value)
+        return padded_src_stentences, padded_trg_stentences_forward, padded_trg_stentences_loss
     
 
 def get_parameters_info(model):
@@ -71,19 +77,19 @@ def get_parameters_info(model):
     return names, trainable, nontrainable
 
 
-def plot_loss(train_class_losses, val_class_losses, plots_dir, model_name):
+def plot_loss(train_class_losses, val_class_losses, steps, plots_dir, model_name):
 
     fig = plt.figure(figsize=(8, 3))
-    epochs = [ep + 1 for ep in range(len(train_class_losses))]
-    plt.plot(epochs, train_class_losses, label="Training Classification Loss")
-    plt.plot(epochs, val_class_losses, label="Validation Classification Loss")
-    plt.xlabel("Epochs")
+    plt.plot(steps, train_class_losses, label="Training Loss")
+    plt.plot(steps, val_class_losses, label="Validation Loss")
+    plt.xlabel("Step")
     plt.ylabel("Loss")
     plt.legend()  # Add a legend to differentiate the lines
     plot_path = os.path.join(plots_dir, f'{model_name}_losses.png')
     plt.savefig(plot_path, dpi=300)
     # Close the plot to prevent it from displaying
     plt.close(fig)
+
 
 def save_checkpoint(model:torch.nn.Module, optimizer, save_dir:str, run_name:str, step:int, in_onnx=False):
     model_path = os.path.join(save_dir, f"{run_name}_step_{step}.pth")
@@ -95,3 +101,10 @@ def save_checkpoint(model:torch.nn.Module, optimizer, save_dir:str, run_name:str
         torch.save({'model_state_dict': model.state_dict(),
                     'optimizer_state_dict': optimizer.state_dict()}, model_path)
     print(f"Checkpoint saved at: {model_path}")
+
+
+def compute_bleu(references, candidate):
+    if type(references[0]) != list:
+        references = [references]
+    bleu = sentence_bleu(references, candidate)
+    return  bleu
