50bd5a2fe79ef13defe8abefedee636e85696eee
Abdelrhman Ashraf <abdoashraf938@yahoo.com>
2025-01-31 15:57:04 +0200

    Save training plots to a specified directory after training completion

--- workflow.py
+++ workflow.py
@@ -98,7 +98,10 @@ if __name__ == '__main__':
                         collator=mycollate, compute_metrics_func=compute_metrics)
 
     history = trainer.train()
-    print("Training Done.")
+    save_plots_dir = os.path.join(args.out_dir, 'plots')
+    os.makedirs(save_plots_dir, exist_ok=True)
+    plot_history(history, save_plots_dir, model_args.model_type)
+    print(f"Training Done, plots saved at {save_plots_dir}")
 
     if args.test_csv_path is not None:
         print("---------------------Start evaluation on test-set...---------------------")
@@ -116,8 +119,3 @@ if __name__ == '__main__':
         test_metrics = trainer.evaluate(dataloader=test_loader, set_name='test')
         print(test_metrics)
         print("evaluation Done.")
-
-    
-    save_plots_dir = os.path.join(args.out_dir, 'plots')
-    os.makedirs(save_plots_dir, exist_ok=True)
-    plot_history(history, save_plots_dir, model_args.model_type)
