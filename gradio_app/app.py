f4687e906a2c3ac3e97a1b29a3a61f8e7504bb13
Abdelrhman Ashraf <abdoashraf938@yahoo.com>
2025-02-01 01:50:44 +0200

    Update model and tokenizer paths in Gradio app to use assets directory

--- gradio_app/app.py
+++ gradio_app/app.py
@@ -15,21 +15,21 @@ def en_translate_ar(text, model, tokenizer, max_tries=50):
 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-tokenizer = Callable_tokenizer('./out/tokenizers/en-ar_tokenizer.model')
+tokenizer = Callable_tokenizer('./assets/tokenizers/en-ar_tokenizer.model')
 
-model_state_dict = torch.load("./out/models/en-ar_s2sAttention.pth", map_location=device, weights_only=True)['model_state_dict']
+model_state_dict = torch.load("./assets/models/en-ar_s2sAttention.pth", map_location=device, weights_only=True)['model_state_dict']
 model_args = ModelArgs('s2sattention', "./Configurations/s2sattention_model_config.json")
 s2sattention = get_model(model_args, len(tokenizer))
 s2sattention.load_state_dict(model_state_dict)
 s2sattention.to(device)
 
-model_state_dict = torch.load("./out/models/en-ar_s2s.pth", map_location=device, weights_only=True)['model_state_dict']
+model_state_dict = torch.load("./assets/models/en-ar_s2s.pth", map_location=device, weights_only=True)['model_state_dict']
 model_args = ModelArgs('s2s', "./Configurations/s2s_model_config.json")
 s2s = get_model(model_args, len(tokenizer))
 s2s.load_state_dict(model_state_dict)
 s2s.to(device)
 
-model_state_dict = torch.load("./out/models/en-ar_transformer.pth", map_location=device, weights_only=True)['model_state_dict']
+model_state_dict = torch.load("./assets/models/en-ar_transformer.pth", map_location=device, weights_only=True)['model_state_dict']
 model_args = ModelArgs('transformer', "./Configurations/transformer_model_config.json")
 transformer = get_model(model_args, len(tokenizer))
 transformer.load_state_dict(model_state_dict)
