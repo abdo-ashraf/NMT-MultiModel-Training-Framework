918d1c18bcfa1a96d1c55aa6064fffcf50d91137
Abdelrhman Ashraf <abdoashraf938@yahoo.com>
2025-02-01 11:59:00 +0200

    Add beam and greedy search translation functions to the Gradio app

--- gradio_app/app.py
+++ gradio_app/app.py
@@ -4,7 +4,11 @@ from Models.AutoModel import get_model
 from gradio_utils import Callable_tokenizer, greedy_decode
 import gradio as gr
 
-def en_translate_ar(text, model, tokenizer, max_tries=50):
+def en_translate_ar_beam(text, model, tokenizer, max_tries=50):
+    return "future work"
+
+
+def en_translate_ar_greedy(text, model, tokenizer, max_tries=50):
     source_tensor = torch.tensor(tokenizer(text)).unsqueeze(0)
     target_tokens = greedy_decode(model, source_tensor,
                                   tokenizer.get_tokenId('<s>'),
@@ -22,25 +26,35 @@ model_args = ModelArgs('s2sattention', "./Configurations/s2sattention_model_conf
 s2sattention = get_model(model_args, len(tokenizer))
 s2sattention.load_state_dict(model_state_dict)
 s2sattention.to(device)
+s2sattention.eval()
 
 model_state_dict = torch.load("./assets/models/en-ar_s2s.pth", map_location=device, weights_only=True)['model_state_dict']
 model_args = ModelArgs('s2s', "./Configurations/s2s_model_config.json")
 s2s = get_model(model_args, len(tokenizer))
 s2s.load_state_dict(model_state_dict)
 s2s.to(device)
+s2s.eval()
 
 model_state_dict = torch.load("./assets/models/en-ar_transformer.pth", map_location=device, weights_only=True)['model_state_dict']
 model_args = ModelArgs('transformer', "./Configurations/transformer_model_config.json")
 transformer = get_model(model_args, len(tokenizer))
 transformer.load_state_dict(model_state_dict)
 transformer.to(device)
+transformer.eval()
 
 
-def lunch(raw_input, maxtries=50):
-    transformer_out = en_translate_ar(raw_input, transformer, tokenizer, maxtries)
-    s2sattention_out = en_translate_ar(raw_input, s2sattention, tokenizer, maxtries)
-    s2s_out = en_translate_ar(raw_input, s2s, tokenizer, maxtries)
+def launch_translation_greedy(raw_input, maxtries=50):
+    transformer_out = en_translate_ar_greedy(raw_input, transformer, tokenizer, maxtries)
+    s2sattention_out = en_translate_ar_greedy(raw_input, s2sattention, tokenizer, maxtries)
+    s2s_out = en_translate_ar_greedy(raw_input, s2s, tokenizer, maxtries)
     return transformer_out, s2sattention_out, s2s_out, 
+
+
+def launch_translation_beam(raw_input, maxtries=50):
+    transformer_out = en_translate_ar_beam(raw_input, transformer, tokenizer, maxtries)
+    s2sattention_out = en_translate_ar_beam(raw_input, s2sattention, tokenizer, maxtries)
+    s2s_out = en_translate_ar_beam(raw_input, s2s, tokenizer, maxtries)
+    return transformer_out, s2sattention_out, s2s_out
 
 
 custom_css ='.gr-button {background-color: #bf4b04; color: white;}'
@@ -59,7 +73,11 @@ with gr.Blocks(css=custom_css) as demo:
             output2 = gr.Textbox(label="Arabic seq2seq with Attention Translation")
             output3 = gr.Textbox(label="Arabic seq2seq No Attention Translation")
             
-            start_btn = gr.Button(value='Arabic Translation', elem_classes=["gr-button"])
-    start_btn.click(fn=lunch, inputs=input_text, outputs=[output1, output2, output3])
+            start_greedy_btn = gr.Button(value='Arabic Translation (Greedy search)', elem_classes=["gr-button"])
+            start_beam_btn = gr.Button(value='Arabic Translation (Beam search)', elem_classes=["gr-button"])
+
+    start_greedy_btn.click(fn=launch_translation_greedy, inputs=input_text, outputs=[output1, output2, output3])
+    start_beam_btn.click(fn=launch_translation_beam, inputs=input_text, outputs=[output1, output2, output3])
+
 
 demo.launch()
